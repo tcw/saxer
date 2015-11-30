8962d8367f5a15cdbb5ead371e3d965229566242
tcw <tcwgit@gmail.com>
2015-11-30 22:09:36 +0100

    Query parser

--- queryParser/queryParser.go
+++ queryParser/queryParser.go
@@ -11,34 +11,42 @@ func Parse(query string) tagPath.TagPath {
 	for _, value := range split {
 		tagText := strings.TrimSpace(value)
 		if len(tagText) != 0 {
-			tag := tagPath.NewTag()
-			if strings.Contains(tagText, "?") {
-				elem := strings.Split(tagText, "?")
-				if len(elem) != 0 {
-					if len(elem[0]) > 0 {
-						tag.Name = elem[0]
-					}
-					if len(elem[1]) > 0 {
-						if strings.Contains(elem[1], "&") {
-							attr := strings.Split(elem[1], "&")
-							for _, val := range attr {
-								if strings.Contains(val, "=") {
-									attrKeyVal := strings.Split(val, "=")
-									tag.Add(tagPath.Attribute{attrKeyVal[0], attrKeyVal[1]})
-								}else {
-									tag.Add(tagPath.Attribute{val,""})
-								}
-							}
-						}else {
-
-						}
-					}
-				}
-			}else {
-				tag.Name = tagText
-			}
-			path.Add(tag)
+			path.Add(getTag(tagText))
 		}
 	}
 	return path
+}
+
+func getTag(tagText string) tagPath.Tag{
+	tag := tagPath.NewTag()
+	if strings.Contains(tagText, "?") {
+		elem := strings.Split(tagText, "?")
+		if len(elem) != 0 {
+			if len(elem[0]) > 0 {
+				tag.Name = elem[0]
+			}
+			if len(elem[1]) > 0 {
+				if strings.Contains(elem[1], "&") {
+					attr := strings.Split(elem[1], "&")
+					for _, val := range attr {
+						tag.Add(getAttribute(val))
+					}
+				}else {
+					tag.Add(getAttribute(elem[1]))
+				}
+			}
+		}
+	}else {
+		tag.Name = tagText
+	}
+	return tag
+}
+
+func getAttribute(attr string) tagPath.Attribute {
+	if strings.Contains(attr, "=") {
+		attrKeyVal := strings.Split(attr, "=")
+		return tagPath.Attribute{attrKeyVal[0], attrKeyVal[1]}
+	}else {
+		return tagPath.Attribute{attr, ""}
+	}
 }
